043372b71b56c6f9b35cd5ddb9fc4c243aab09bb
master <spikerus93@rambler.ru>
2023-06-16 21:47:44 +0300

    Решение задач  по 3-ому семинару С#

---
+++ task18/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая по заданному номеру
+// четверти, показывает диапазон возможных координат
+// точек в этой четверти (x и y).
+
+Console.WriteLine("Введите № Координатной четверти от 1 до 4: ");
+int quater = Console.ToInt32(Console.ReadLine());
+
+if(quater ==1)
+{
+    Console.WriteLine("x>0, y>0");
+}
+else if(quater ==2)
+{
+    Console.WriteLine("x<0, y>0");
+}
+else if(quater ==3)
+{
+    Console.WriteLine("x<0, y<0");
+}
+else if(quater ==4)
+{
+    Console.WriteLine("x>0, y>0");
+}
+else if(quater =="Нет такой четверти")
+{
+    Console.WriteLine("x<0, y>0");
+}
---
+++ task20/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход
+// координаты двух точек и находит расстояние между
+// ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.WriteLine("Введите Х первой точки:");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y первой точки:");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Х второй точки:");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y второй точки:");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double  skobX = Math.Pow(x1 - x2, 2);
+double  skobY = Math.Pow(y1 - y2, 2);
+double result = Math.Sqrt(skobX+skobY);
+
+// Console.WriteLine(result);
+
+Console.WriteLine($"{result:f2}");
